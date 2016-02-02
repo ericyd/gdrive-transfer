a5cb4893d3223d17126b62c5cb55925b7ddf999d
Eric Dauenhauer <eric_yd@yahoo.com>
2016-02-01 17:38:34 -0800

    fixed event bindings for new methods

--- src/js/init.js
+++ src/js/init.js
@@ -4,26 +4,29 @@
 
 var $ = jQuery = require('jquery');
 require('../../node_modules/bootstrap-sass/assets/javascripts/bootstrap/button.js');
-require('../../node_modules/bootstrap-sass/assets/javascripts/bootstrap/modal.js');
 require('../../node_modules/jquery-ui/effect-blind.js');
 var getValues = require('./getValues');
+var picker = require('./picker');
 
 $(document).ready(function() {
-  $("#working").hide();
-  $("#notes").hide();
   $("#status").hide();
   $("#troubleshooting").hide();
-  $("#important").hide();
   $("#complete").hide();
-
 });
 
 $("#thisForm").submit(function( event ) {
   // Bootstrap button action binding
   var $btn = $("#transferFolderButton").button('loading');
-  //$btn.button('reset') // call to reset to original condition
   $("#description").hide("blind");
   $("#status").show("blind");
-  getValues.run();
+  getValues.get();
   event.preventDefault();
 });
+
+$("#selectFolderButton").click(function() {
+  picker.showPicker();
+})
+
+$("#confirm-button").click(function() {
+  getValues.confirm();
+})
