e45c133a9d81dd27e7710754a135d6fa084ad776
Eric Dauenhauer <eric_yd@yahoo.com>
2016-02-01 17:37:45 -0800

    modularized script and changed arguments to accept selectedFolder object

--- src/js/getFolders.js
+++ src/js/getFolders.js
@@ -1,9 +1,19 @@
 var $ = jQuery = require('jquery');
+var transferFolder = require('./transferFolder');
 /* 
-   
+   This function retrieves the folders as an array and 
+   passes them to transferFolder individually
 */
 
-function getFolders(folderId, folderArray, folderName, newOwner) { 
+exports.run = function(selectedFolder) { 
+  var folderArray = [];
+  var newOwner = theForm.newOwner.value;
+  
+  var folderId = selectedFolder.id;
+      
+  var folderName = selectedFolder.name;
+  
+  
   // Add the top folder to the folderArray
   var pair = [];
   pair.push(folderId);
@@ -16,12 +26,12 @@ function getFolders(folderId, folderArray, folderName, newOwner) {
     .withSuccessHandler(function(folderArray) {
 
       // increments the counter of folders
-      counter = folderArray.length;
+      var counter = folderArray.length;
       document.getElementById("notes").innerHTML = document.getElementById("notes").innerHTML + '<br />Transferring folders ... ';
       
       // Iterate through the Folder Array
       for (i = 0; i < folderArray.length; i++) {
-        transferFolder(folderArray[i][0], folderArray[i][1], folderArray, folderArray[0][0]);
+        transferFolder.transfer(folderArray[i][0], folderArray[i][1], folderArray, folderArray[0][0]);
       }
     })
     .withFailureHandler(function(msg) {
