8d628dd20994270026ed157b6dc1e0177bc30439
Eric Dauenhauer <eric_yd@yahoo.com>
2016-02-01 18:03:07 -0800

    fill #status-table when transferring folders

--- src/js/getFolders.js
+++ src/js/getFolders.js
@@ -22,28 +22,45 @@ exports.run = function(selectedFolder) {
   folderArray.push(pair);
 
   google.script.run
-    // folderArray is an array of folderId strings
+    // folderArray is an array of arrays
+    // each element of folderArray has 2 elements
+    // the first element is the folder ID
+    // the second element is a string representing the folder path
     .withSuccessHandler(function(folderArray) {
-
+      
+      
       // increments the counter of folders
       var counter = folderArray.length;
-      document.getElementById("notes").innerHTML = document.getElementById("notes").innerHTML + '<br />Transferring folders ... ';
+      var statusTable;
+      
+      // Update status for user
+      $("#status-title").html("Transferring folders <i class='fa fa-spinner fa-spin'></i>");
+      
       
       // Iterate through the Folder Array
+      // Note: order doesn't matter, as long as the top folder is transferred first
+      // the folder structure is not affected in this process, so no need to preserve hierarchy of folders
       for (i = 0; i < folderArray.length; i++) {
+        
+        // build status-table row
+        statusTable = "";
+        statusTable += "<tr>";
+        // path
+        statusTable += "<td>" + folderArray[i][1] + "</td>";
+        // id
+        statusTable += "<td id='" + folderArray[i][0] + "'><i>Waiting...</i></td>";
+        statusTable += "</tr>";
+        $(statusTable).hide().appendTo("#status-table").show('blind');
+        
         transferFolder.transfer(folderArray[i][0], folderArray[i][1], folderArray, folderArray[0][0]);
       }
     })
     .withFailureHandler(function(msg) {
-      document.getElementById("notes").innerHTML = document.getElementById("notes").innerHTML + '<br />' + 'failed getting sub-folders: ' + msg;
-      
-      // increments the counter of folders
-      counter = folderArray.length;
-      
-      // Iterate through the Folder Array
-      for (i = 0; i < folderArray.length; i++) {
-        transferFolder(folderArray[i][0], folderArray[i][1], folderArray, folderArray[0][0]);
-      }
+      var errormsg = "<div class='alert alert-danger' role='alert'><b>Error:</b> There was an error creating folder structure.<br />";
+      errormsg += "<b>Error message:</b> " + msg + ".<br>";
+      errormsg += "Please try again. Make sure you have correct permissions to copy this folder, and please input the entire sharing URL, not just the folder ID</div>";
+      $("#errors").append(errormsg);
+      $("#status-title").html("Error");
       
     })
     .getFolders(folderId, folderArray, newOwner);
