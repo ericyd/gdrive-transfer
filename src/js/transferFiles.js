7d71e8ce40f0146900eae8f7633ec3bdf744fe4e
Eric Dauenhauer <eric_yd@yahoo.com>
2016-02-01 23:48:37 -0800

    improved program flow; eliminated counter and created quasi-recursive structure

--- src/js/transferFiles.js
+++ src/js/transferFiles.js
@@ -6,41 +6,51 @@
 
 var $ = jQuery = require('jquery');
 require('../../node_modules/jquery-ui/effect-blind.js');
+var getFolders = require('./getFolders');
+var transferFolder = require('./transferFolder');
 
-exports.files = function(folderArray, topFolderId) {
+exports.files = function(folderId, folderArray) {
     
-    var pair = folderArray.shift();
-    var folderId = pair[0];
-    var folderName = pair[1];
+    var newOwner = getFolder.getNewOwner();
+    
+    
     
     $("#" + folderId).html("Transferring files <i class='fa fa-spinner fa-spin'></i>").addClass("disabled");
     
+    
+    
     google.script.run
-      .withSuccessHandler(function(folderName) {
+      .withSuccessHandler(function() {
         // Respond to success conditions here
         $("#" + folderId).html("Complete").addClass("bg-success");
         
         if (folderArray.length > 0) {
-          // Recurse
-          transferFiles(folderArray, topFolderId);
+          // Call with new folderArray
+          transferFolder.transfer(folderArray);
+          
         } else {
           // Done!
-          $("#troubleshooting").append("<a href='https://drive.google.com/open?id=" + topFolderId + "' target='_blank'>https://drive.google.com/open?id=" + topFolderId + "</a>").show('blind');
+          $("#troubleshooting").show('blind');
+          
         }
       })
+      
       .withFailureHandler(function(msg) {
-        // Respond to failure conditions here.
+        // Generate error message
         var errorMsg = "<b>Error:</b> Failed to transfer files in one folder. The may be because you don't own all files, or because the folder contains a synced file (e.g. PDF, image, HTML file, etc.).<br /><b>Error message:</b> " + msg + ".";
         $("#errors").append("<div class='alert alert-danger' role='alert'>" + errorMsg + "</div>");
         
         if (folderArray.length > 0) {
-          // Recurse
-          transferFiles(folderArray, topFolderId);
+          // Call with new folderArray
+          transferFolder.transfer(folderArray);
+          
         } else {
           // Done!
-          $("#troubleshooting").append("<a href='https://drive.google.com/open?id=" + topFolderId + "' target='_blank'>https://drive.google.com/open?id=" + topFolderId + "</a>").show('blind');
+          $("#troubleshooting").show('blind');
+          
         }
         
       })
+      
       .transferFiles(folderId, newOwner);
 }
