bdc084d95bdfda6dd8fc0de34186a39bbf7e3beb
Eric Dauenhauer <eric_yd@yahoo.com>
2016-02-01 23:49:36 -0800

    exports newOwner to reduce number of passed arguments to transfer functions

--- src/js/getFolders.js
+++ src/js/getFolders.js
@@ -1,13 +1,22 @@
 var $ = jQuery = require('jquery');
 var transferFolder = require('./transferFolder');
 /* 
-   This function retrieves the folders as an array and 
-   passes them to transferFolder individually
+    This function retrieves the folders as an array and 
+    passes them to transferFolder individually
+   
+    Note: order doesn't matter, as long as the top folder is transferred first
+    the folder structure is not affected in this process, so no need to preserve hierarchy of folders while calling the function
 */
+
+var newOwner;
+
+exports.getNewOwner = function() {
+  return newOwner;
+}
 
 exports.run = function(selectedFolder) { 
   var folderArray = [];
-  var newOwner = theForm.newOwner.value;
+  newOwner = theForm.newOwner.value;
   
   var folderId = selectedFolder.id;
       
@@ -27,33 +36,33 @@ exports.run = function(selectedFolder) {
     // the first element is the folder ID
     // the second element is a string representing the folder path
     .withSuccessHandler(function(folderArray) {
+      var topFolderId = folderArray[0][0];
+      $("#troubleshooting").append("<a href='https://drive.google.com/open?id=" + topFolderId + "' target='_blank'>https://drive.google.com/open?id=" + topFolderId + "</a>")
       
       
-      // increments the counter of folders
-      var counter = folderArray.length;
-      var statusTable;
+      
       
       // Update status for user
       $("#status-title").html("Transferring folders <i class='fa fa-spinner fa-spin'></i>");
       
-      
-      // Iterate through the Folder Array
-      // Note: order doesn't matter, as long as the top folder is transferred first
-      // the folder structure is not affected in this process, so no need to preserve hierarchy of folders
+      // build status-table rows
+      var statusTable = "";
       for (i = 0; i < folderArray.length; i++) {
-        
-        // build status-table row
-        statusTable = "";
+
         statusTable += "<tr>";
         // path
         statusTable += "<td>" + folderArray[i][1] + "</td>";
         // id
         statusTable += "<td id='" + folderArray[i][0] + "'><i>Waiting...</i></td>";
         statusTable += "</tr>";
+
         $(statusTable).hide().appendTo("#status-table").show('blind');
-        
-        transferFolder.transfer(folderArray[i][0], folderArray[i][1], folderArray, folderArray[0][0]);
+
       }
+      
+      transferFolder.transfer(folderArray);
+      
+      
     })
     .withFailureHandler(function(msg) {
       var errormsg = "<div class='alert alert-danger' role='alert'><b>Error:</b> There was an error creating folder structure.<br />";
