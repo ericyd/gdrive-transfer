6b58130a10607798a8118a2bc7360a51d3796d76
Eric Dauenhauer <eric_yd@yahoo.com>
2016-02-01 18:03:37 -0800

    updated to jQuery and improved error messages

--- src/js/transferFiles.js
+++ src/js/transferFiles.js
@@ -1,44 +1,44 @@
 /* 
-   This transfers ownership of all the files from each folder ID
+   This transfers ownership of all the files in a single function
+   
+   todo: maybe break up into each individual folder?
 */
 
 var $ = jQuery = require('jquery');
+require('../../node_modules/jquery-ui/effect-blind.js');
 
-
-function transferFiles(folderArray, topFolderId) {
+exports.files = function(folderArray, topFolderId) {
     
     var pair = folderArray.shift();
     var folderId = pair[0];
     var folderName = pair[1];
     
-    document.getElementById("notes").innerHTML = document.getElementById("notes").innerHTML + '<br />Transferring files in "' + folderName + '" ... ';
+    $("#" + folderId).html("Transferring files <i class='fa fa-spinner fa-spin'></i>").addClass("disabled");
     
     google.script.run
       .withSuccessHandler(function(folderName) {
-        // Respond to success conditions here.
-        document.getElementById("notes").innerHTML = document.getElementById("notes").innerHTML + '<b>Done</b>';
+        // Respond to success conditions here
+        $("#" + folderId).html("Complete").addClass("bg-success");
         
         if (folderArray.length > 0) {
+          // Recurse
           transferFiles(folderArray, topFolderId);
         } else {
-          document.getElementById("working").style.display = "none";
-          document.getElementById("complete").style.display = "block";
-          document.getElementById("troubleshooting").innerHTML = document.getElementById("troubleshooting").innerHTML + "<a href='https://drive.google.com/open?id=" + topFolderId + "' target='_blank'>https://drive.google.com/open?id=" + topFolderId + "</a>";
-          document.getElementById("troubleshooting").style.display = "block";
+          // Done!
+          $("#troubleshooting").append("<a href='https://drive.google.com/open?id=" + topFolderId + "' target='_blank'>https://drive.google.com/open?id=" + topFolderId + "</a>").show('blind');
         }
       })
       .withFailureHandler(function(msg) {
         // Respond to failure conditions here.
-        document.getElementById("notes").innerHTML = document.getElementById("notes").innerHTML + "Failed to transfer files in one folder. The may be because you don't own all files, or because the folder contains a synced file (e.g. PDF, image, HTML file, etc.)";
+        var errorMsg = "<b>Error:</b> Failed to transfer files in one folder. The may be because you don't own all files, or because the folder contains a synced file (e.g. PDF, image, HTML file, etc.).<br /><b>Error message:</b> " + msg + ".";
+        $("#errors").append("<div class='alert alert-danger' role='alert'>" + errorMsg + "</div>");
         
         if (folderArray.length > 0) {
+          // Recurse
           transferFiles(folderArray, topFolderId);
         } else {
-          document.getElementById("working").style.display = "none";
-          document.getElementById("complete").style.display = "block";
-          document.getElementById("troubleshooting").innerHTML = document.getElementById("troubleshooting").innerHTML + "<a href='https://drive.google.com/open?id=" + topFolderId + "' target='_blank'>https://drive.google.com/open?id=" + topFolderId + "</a>";
-          document.getElementById("troubleshooting").style.display = "block";
-          
+          // Done!
+          $("#troubleshooting").append("<a href='https://drive.google.com/open?id=" + topFolderId + "' target='_blank'>https://drive.google.com/open?id=" + topFolderId + "</a>").show('blind');
         }
         
       })
