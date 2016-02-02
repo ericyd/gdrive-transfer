f155b3e193298d5421c06118138e72b6f47fe1c3
Eric Dauenhauer <eric_yd@yahoo.com>
2016-02-01 18:03:51 -0800

    improved status reporting

--- src/js/transferFolder.js
+++ src/js/transferFolder.js
@@ -1,17 +1,20 @@
 var $ = jQuery = require('jquery');
-
+var transferFiles = require('./transferFiles');
 
 exports.transfer = function (folderId, folderName, folderArray, topFolderId) {
   
+  
+  $("#" + folderId).html("Transferring folder <i class='fa fa-spinner fa-spin'></i>").addClass("disabled");
+  
   google.script.run
     .withSuccessHandler(function(folderName) {            
+        
         
         // decrements the counter so that copyFiles will fire only when all folders have been created
         counter = counter - 1;
         
         if (counter === 0) {
-          document.getElementById("notes").innerHTML = document.getElementById("notes").innerHTML + '<b>Done</b>';
-          transferFiles(folderArray, topFolderId);
+          transferFiles.files(folderArray, topFolderId);
         }
     })
     .withFailureHandler(function(msg) {
