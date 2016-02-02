177a9563a44ea901eb70304193fc212e29dc6cac
Eric Dauenhauer <eric_yd@yahoo.com>
2016-02-01 16:49:44 -0800

    eliminating redundant function declarations

--- src/gs/get.js
+++ src/gs/get.js
@@ -1,27 +1,3 @@
-/***********************************************
-doGet function required by Google Script Web App
-***********************************************/
-
-/**
- * Serves HTML of the application for HTTP GET requests.
- * If folderId is provided as a URL parameter, the web app will list
- * the contents of that folder (if permissions allow). Otherwise
- * the web app will list the contents of the root folder.
- *
- * @param {Object} e event parameter that can contain information
- *     about any URL parameters provided.
- */
-function doGet(e) {
-  var template = HtmlService.createTemplateFromFile('Index');
- 
-  template.thisForm = e.parameter.thisForm;
-  
-  // Build and return HTML in IFRAME sandbox mode.
-  return template.evaluate()
-      .setTitle('Transfer ownership for a Google Drive folder')
-      .setSandboxMode(HtmlService.SandboxMode.IFRAME);
-}
-
 
 /**
  * Get contents of folder and copy to new folder
@@ -66,56 +42,4 @@ function getFolders(folderId, folderArray, newOwner) {
   }
   
   return folderArray;
-}
-
-
-// Transfers folder with folderId (string) to newOwner (string)
-function transferFolder(folderId, newOwner) {
-  var folder = DriveApp.getFolderById(folderId);
-  var folderName = folder.getName();
-  var activeUser = Session.getActiveUser().getEmail();
-  
-  // Only attempt to transfer if you own the file
-  if (folder.getOwner().getEmail() == activeUser) {
-    folder.setOwner(newOwner);
-  }
-  
-  return folderName;
-}
-
-
-// Transfers files from folderId (string) to newOwner (string)
-function transferFiles(folderId, newOwner) {
-  var folder = DriveApp.getFolderById(folderId);
-  var folderName = folder.getName();
-  var files = folder.getFiles();
-  var activeUser = Session.getActiveUser().getEmail();
-  
-  while (files.hasNext()) {
-    var file = files.next();
-     
-    // Only attempt to transfer if you own the file
-    if (file.getOwner().getEmail() == activeUser) {
-      file.setOwner(newOwner);
-    }
-  } 
-  
-  return folderName;
-}
-
-
-
-// fullPath is a string that begins empty and gets appended 
-function getFullPath(fullPath, folderId) {
-  var parents = DriveApp.getFolderById(folderId).getParents();
-  var parent = parents.next();
-  
-  if (parent.getName() != "My Drive") {
-    fullPath = parent.getName() + " > " + fullPath;
-    
-    if (parent.getParents().hasNext()) {
-      fullPath = getFullPath(fullPath, parent.getId());
-    }
-  }
-  return fullPath;
 }
