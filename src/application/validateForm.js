3b10b862a3128bcc56e57e4e83c12df1e49e84cb
Eric Dauenhauer <eric@ericyd.com>
2016-12-27 12:54:48 -0800

    Add validation for when folder is not shared with new owner

--- src/application/validateForm.js
+++ src/application/validateForm.js
@@ -2,12 +2,27 @@
  * Returns number of existing triggers for user.
  * @return {number} triggers the number of active triggers for this user
  */
-function validateForm() {
+function validateForm(selectedFolder) {
     var numberOfTriggers = ScriptApp.getProjectTriggers().length;
-    var isShared = false;
+    var isShared = isEmailInEditors(selectedFolder.srcId, selectedFolder.newOwner);
 
     return {
         number: numberOfTriggers,
         isShared: isShared
     };
+}
+
+function isEmailInEditors(fileId, email) {
+    var editors = DriveApp.getFileById(fileId).getEditors();
+    var viewers = DriveApp.getFileById(fileId).getViewers();
+    var i;
+    for (i = 0; i < editors.length; i++) {
+        if (editors[i].getEmail() === email) return true;
+    }
+
+    for (i = 0; i < viewers.length; i++) {
+        if (viewers[i].getEmail() === email) return true;
+    }
+
+    return false;
 }
